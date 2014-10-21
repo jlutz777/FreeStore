76a1484b8db3177d55315c439bce6fc1ae1bd957
Jason Lutz <jlutz777@gmail.com>
2014-10-21 02:56:23 +0000

    fix 404

--- run_freestore.py
+++ run_freestore.py
@@ -1,5 +1,5 @@
 import bottle
-from bottle import HTTPError
+from bottle import HTTPError, HTTPResponse
 from bottle.ext import sqlalchemy
 from sqlalchemy import create_engine, Column, Integer, Sequence, Unicode, DateTime
 from sqlalchemy.ext.declarative import declarative_base
