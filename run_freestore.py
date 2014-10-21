54a63f2fbd7160ba70a3c7fd895bb60fbc4bcfd7
Jason Lutz <jlutz777@gmail.com>
2014-10-21 02:47:53 +0000

    add a couple more columns to the view

--- run_freestore.py
+++ run_freestore.py
@@ -34,7 +34,7 @@ class CustomerFamily(Base):
 def show(db):
     entity = db.query(CustomerFamily).first()
     if entity:
-        return {'id': entity.id, 'name': entity.email}
+        return {'id': entity.id, 'name': entity.email, 'city': entity.city, 'zip': entity.zip}
     return HTTPError(404, 'Entity not found.')
 
 if __name__ == "main":
