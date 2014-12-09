d9768918b857a1f17a8ecb17fd3255affc8d8e33
Jason Lutz <jlutz777@gmail.com>
2014-12-09 17:33:30 -0500

    whatever

--- forms/checkout.py
+++ forms/checkout.py
@@ -1,59 +1,54 @@
 from wtforms import Form, validators, DateField, BooleanField, StringField, HiddenField, FormField
 from wtforms_alchemy import ModelForm, ModelFieldList
 
-from models import CustomerFamily, Dependent
+from models import ShoppingItem, Visit, ShoppingCategory
 
-'''
-visit
-- checkin (hidden)
-- checkout (datetime.now)
-- family_id (hidden)
-- shopping_item
-  - id, name
-  - category
-     - id, name, dailyLimit
-'''
+class CategoryForm(ModelForm):
+    class Meta:
+        model = ShoppingCategory
+        include = ['id']
+        field_args = {
+            'id': {
+                'validators': [validators.InputRequired()]
+            },
+            'name': {
+                'validators': [validators.Optional()]
+            },
+            'dailyLimit': {
+                'validators': [validators.Optional()]
+            }
+        }
 
 class ShoppingItemForm(ModelForm):
     class Meta:
         datetime_format = '%m/%d/%Y'
-        model = Dependent
-        include = ['id']
+        model = ShoppingItem
         field_args = {
-            'id' : {
+            'name': {
                 'validators' : [validators.Optional()]
-            },
-            'firstName': {
-                'validators' : [validators.InputRequired()]
-            },
-            'lastName': {
-                'validators' : [validators.InputRequired()]
-            },
-            'birthdate': {
-                'validators' : [validators.InputRequired()]
             }
         }
+
+    category = FormField(CatgoryForm)
 
 class CheckoutForm(ModelForm):
     class Meta:
         datetime_format = '%m/%d/%Y'
-        model = CustomerFamily
+        model = Visit
+        include = ['id']
         field_args = {
             'id' : {
+                'validators' : [validators.InputRequired()]
+            },
+            'checkin': {
+                'validators' : [validators.InputRequired()]
+            },
+            'checkout': {
                 'validators' : [validators.Optional()]
             },
-            'city': {
+            'family_id': {
                 'validators' : [validators.InputRequired()]
-            },
-            'state': {
-                'validators' : [validators.InputRequired()]
-            },
-            'zip': {
-                'validators' : [validators.InputRequired()]
-            },
-            'datecreated': {
-                'validators' : [validators.Optional()]
             }
         }
 
-    dependents = ModelFieldList(FormField(DependentForm), min_entries=1)
+    items = ModelFieldList(FormField(ShoppingItemForm), min_entries=0)
