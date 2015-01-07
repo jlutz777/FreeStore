684c208bfec727cd66bd4a2eccedbfeea1c59d0a
Jason Lutz <jlutz777@gmail.com>
2015-01-06 22:06:25 -0500

    Add quantity to shopping items

---
+++ alembic/versions/32ccd6db0955_add_quantity_to_shopping_items.py
@@ -0,0 +1,22 @@
+"""Add quantity to shopping items
+
+Revision ID: 32ccd6db0955
+Revises: 430dc4ed6dd6
+Create Date: 2015-01-06 21:59:03.761529
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '32ccd6db0955'
+down_revision = '430dc4ed6dd6'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
