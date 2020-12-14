e0a65dcd97be892e57f3e0eb775d7735ff059bb3
jlutz777 <jlutz777@gmail.com>
2020-12-14 19:52:11 +0000

    Add report for volunteers per day, order the visits

--- reporting/utils.py
+++ reporting/utils.py
@@ -6,7 +6,7 @@ from .reports import FamilyTotalOverTimeReport, IndividualsByAgeReport
 from .reports import FamilyCheckoutsPerWeekReport, DependentCheckoutsPerWeekReport
 from .reports import EmptyFamilyCheckoutsPerWeekReport, FamilyCheckInsPerWeekReport
 from .reports import FamiliesPerZipReport, CheckoutFrequencyPerMonthReport
-from .reports import VolunteersHoursWorkedReport
+from .reports import VolunteersHoursWorkedReport, VolunteersPerDayReport
 #from .reports import DependentsTotalOverTimeReport, ItemsPerCategoryPerMonthReport
 
 availableReports = {}
@@ -19,8 +19,9 @@ availableReports[6] = IndividualsByAgeReport
 availableReports[7] = FamiliesPerZipReport
 availableReports[8] = CheckoutFrequencyPerMonthReport
 availableReports[9] = VolunteersHoursWorkedReport
-#availableReports[10] = DependentsTotalOverTimeReport
-#availableReports[11] = ItemsPerCategoryPerMonthReport
+availableReports[10] = VolunteersPerDayReport
+#availableReports[11] = DependentsTotalOverTimeReport
+#availableReports[12] = ItemsPerCategoryPerMonthReport
 
 def determineAndCreateReport(report_num, startDate='', endDate=''):
     """Determine the report"""
